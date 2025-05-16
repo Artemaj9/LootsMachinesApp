8709cb3739c5be9b75bd1cec58ee5ebb2f101522
ArtemMoiseev <moiseev@physics.msu.ru>
2025-05-16 09:30:01 +0300

    Initial Commit

---
+++ LootsMachinesApp/LootsMachinesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LootsMachinesAppApp.swift
+//  LootsMachinesApp
+//
+//  Created by Artem on 16.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct LootsMachinesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
