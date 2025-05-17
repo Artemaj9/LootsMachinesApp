4ca69fdf06f99700b4b449ed1ba0c6506e266564
ArtemMoiseev <moiseev@physics.msu.ru>
2025-05-18 00:18:22 +0300

    add dailybonus

--- LootsMachinesApp/Screens/DailyBonus.swift
+++ LootsMachinesApp/Screens/DailyBonus.swift
@@ -5,11 +5,49 @@
 import SwiftUI
 
 struct DailyBonus: View {
-    var body: some View {
-        Text("Daily Bonus")
+  @EnvironmentObject var vm: GameViewModel
+  @State private var startAnimation = false
+  
+  var body: some View {
+    ZStack {
+      Color(hex: "1C0035").opacity(0.8)
+        .ignoresSafeArea()
+      
+      Image(.bonus)
+        .resizableToFit()
+        .hPadding()
+      
+      Image(.bgrays)
+        .resizableToFit()
+        .opacity(0.5)
+        .mask {
+          RadialGradient(stops: [.init(color: .white, location: 0), .init(color: .white.opacity(0), location: 0.6)], center: .center, startRadius: 10, endRadius: startAnimation ? 300 : 10)
+        }
+        .blendMode(.luminosity)
+        .scaleEffect(startAnimation ? 2 : 1)
+        .animation(.easeIn(duration: 3),value: startAnimation)
+            
+      Image(.mysteriousglow)
+        .resizableToFill()
+        .clipped()
+        .opacity(startAnimation ? 0.7 : 0)
+        .blendMode(.screen)
+        .blendMode(.luminosity)
+        .animation(.easeIn(duration: 1),value: startAnimation)
+
+      Text("TAP TO CONTINUE")
+        .lootsFont(size: 18, style: .killjoy, color: .white)
+        .yOffset(vm.h*0.35)
+        .transparentIfNot(startAnimation)
+        .animation(.easeIn(duration: 1).delay(0.7), value: startAnimation)
     }
+    .onAppear {
+      startAnimation = true
+    }
+  }
 }
 
 #Preview {
-    DailyBonus()
+  DailyBonus()
+    .vm
 }
